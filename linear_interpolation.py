f541fdafaa563f260a443375457a64599534afc4
David Chau <86276480+jfiewogh@users.noreply.github.com>
2026-09-03 09:57:13 -0500

    update part 1
    Co-authored-by: valleleo <valleleo@users.noreply.github.com>
    Co-authored-by: zymrk-oss <zymrk-oss@users.noreply.github.com>

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -3,48 +3,60 @@
 # "I have not given or received any unauthorized aid on this assignment."
 #
 # Names:    David Chau
-#           Zymraan
-#           Leo
+#           Zymraan Khan
+#           Leonardo Valle Gomez
 # Section: 571
 # Assignment: Lab 2
 # Date: 1 September 2026
 #
 
-"""
-y = (slope)(x - x1) + y1
-
-slope = (y2 - y1) / (x2 - x1)
-
-y(x) = ((y2 - y1) / (x2 - x1)) * (x - x1) + y1
-
-(10, 2030) = (x1, y1)
-(55, 23030) = (x2, y2)
-
-"""
-
-# Part 1
-print("Part 1:")
-
-slope= (23030-2030)/45
-y1=2030
-x1=10
+from math import *
 
 """
 Part 1:
 For t = 25 minutes, the position p = 9030.0 kilometers
+
+y = (slope)(x - x1) + y1
+slope = (y2 - y1) / (x2 - x1)
+y = (y2 - y1) / (x2 - x1) * (x - x1) + y1
+"""
+
+print("Part 1:")
+
+# time t1 = 10 minutes
+t1 = 10
+# position p1 = 2030 kilometers
+p1 = 2030
+
+# time t2 = 55 minutes
+t2 = 55
+# position p2 = 23030 kilometers
+p2 = 23030
+
+slope = (p2 - p1) / (t2 - t1)
+
+# time t = 25 minutes
+t = 25
+# Use linear interpolation to estimate position p at t = 25 minutes
+p = slope * (t - t1) + p1
+
+print("For t =", t, "minutes, the position p =", p, "kilometers")
+
+
+"""
 Part 2:
 For t = 300 minutes, the position p = 10223.078642554414 kilometers
 """
 
-t = 25
-f=slope*(t-x1)+y1
-print("For t =", t, "minutes, the position p =", f, "kilometers")
-
-
-
-
-
-# Part 2
 print("Part 2:")
 
+radius = 6745
 
+# circumference of circle = 2 * pi * radius
+circumference = 2 * pi * radius
+
+t = 300
+
+p = slope * (t - t1) + pi
+
+print(p)
