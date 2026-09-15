0c4df1159a770c0a7aefb4f53ceef257f09fe596
valleleo <valleleo@tamu.edu>
2026-09-15 09:59:07 -0500

    Improve formatting of quadratic equation output
    Refactor equation string construction for clarity and correctness.

--- lab 4/pretty_equation.py
+++ lab 4/pretty_equation.py
@@ -13,33 +13,38 @@ from math import *
 A=float(input("Please enter the coefficient A: "))
 B=float(input("Please enter the coefficient B: "))
 C=float(input("Please enter the coefficient C: "))
-print()
+
+eq= "The quadratic equation is "
 if A==0:
-    a=str()
+    eq += ""
 elif A==1:
-    a=str("x^2")
+    eq += str("x^2 ")
 elif A==-1:
-    a=str("- x^2")
+    eq += str("- x^2 ")
 elif A<0:
-    a=str(f"- {int(abs(A))}x^2")
+    eq += str(f"- {int(abs(A))}x^2 ")
 elif A>1:
-    a=str(f"{int(A)}x^2")
+    eq += str(f"{int(A)}x^2 ")
 if B==0:
-    b=str()
-elif B==1:
-    b=str("+ x")
+    eq += str()
+elif B==1 and A==0: #gets rid of the + sign if A is 0
+    eq += str("x ")
+elif B==1 and A!=0:
+    eq += str("+ x ")
 elif B==-1:
-    b=str("- x")
+    eq += str("- x ")
 elif B<0:
-    b=str(f"- {int(abs(B))}x")
-elif B>1:
-    b=str(f"+ {int(B)}x")
+    eq += str(f"- {int(abs(B))}x ")
+elif B>1 and A==0: #gets rid of the + sign if A is 0
+    eq += str(f"{int(B)}x ")
+elif B>1 and A!=0:
+    eq += str(f"+ {int(B)}x ")
 if C==0:
-    c=str()
+    eq += str()
 elif C==1:
-    c=str("+ 1")
+    eq += str("+ 1 ")
 elif C<0:
-    c=str(f"- {int(abs(C))}")
+    eq += str(f"- {int(abs(C))} ")
 elif C>1:
-    c=str(f"+ {int(C)}")
-print(f"The quadratic equation is {a} {b} {c} = 0")
+    eq += str(f"+ {int(C)} ")
+print(eq + "= 0")
